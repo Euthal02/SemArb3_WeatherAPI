f184638b5e072d3a52e06480b4dae3f2f2fef14e
Dennis Buathong <dennis.buathong@hotmail.com>
2024-06-05 09:11:44 +0200

    Erste Anpassungen am Frontend Design

--- frontend/app/src/views/HomeView.vue
+++ frontend/app/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Wetter API Logo" src="../assets/WeatherAPI_Logo.png">
+    <HelloWorld msg="Willkommen bei unserer WetterAPI App"/>
   </div>
 </template>
 
