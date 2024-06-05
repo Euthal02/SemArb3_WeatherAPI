49b611c126d34cc78dadd5377d93b4eab3b3dbf2
Dennis Buathong <dennis.buathong@hotmail.com>
2024-06-05 09:24:55 +0200

    Bearbeitung Frontend

--- frontend/app/src/components/HelloWorld.vue
+++ frontend/app/src/components/HelloWorld.vue
@@ -2,9 +2,8 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      Bitte klicken Sie hier um ihre persönliche Wetterprognose zu erhalten.<br>
+      <button>Wetterdaten berechnen</button>
     </p>
   </div>
 </template>
