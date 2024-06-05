efe202eef4a932306722d6605bf772056be73f87
Dennis Buathong <dennis.buathong@hotmail.com>
2024-06-05 10:37:38 +0200

    Testing Template

--- frontend/app/src/views/HomeView.vue
+++ frontend/app/src/views/HomeView.vue
@@ -2,17 +2,29 @@
   <div class="home">
     <img alt="Wetter API Logo" src="../assets/WeatherAPI_Logo.png">
     <HelloWorld msg="Willkommen bei unserer WetterAPI App"/>
+    <h1>Willkommen bei unserer WetterAPI App</h1>
+    <p>
+      Bitte klicken Sie hier um ihre persönliche Wetterprognose zu erhalten.<br>
+      <button @click="getLocation()">Wetterdaten berechnen</button><br>
+      {{ lat }} , {{ lng }}
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+import {ref} from 'vue'
+
+const lat = ref(0)
+const lng = ref(0)
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      lat.value = position.coords.latitude
+      lng.value = position.coords.longitude
+    })
   }
 }
+
 </script>
