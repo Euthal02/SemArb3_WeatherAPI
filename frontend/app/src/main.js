d6bb4fc26ff8cb60c2d020bbb48b1600d73f2ccd
Dennis Buathong <dennis.buathong@hotmail.com>
2024-06-05 10:31:38 +0200

    Update Main.js

--- frontend/app/src/main.js
+++ frontend/app/src/main.js
@@ -1,10 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import Vue3Geolocation from 'vue3-geolocation';
 
-
-app.use(VueGeolocation);
 createApp(App).use(router).mount('#app')
 
 
