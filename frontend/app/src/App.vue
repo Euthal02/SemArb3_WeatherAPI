4988ec3b9bbdd8f63dc7558e181f20554c678c4a
Dennis Buathong <dennis.buathong@hotmail.com>
2024-06-14 16:41:23 +0200

    Styling Frontend

--- frontend/app/src/App.vue
+++ frontend/app/src/App.vue
@@ -1,8 +1,4 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view/>
 </template>
 
@@ -25,6 +21,6 @@ nav a {
 }
 
 nav a.router-link-exact-active {
-  color: #CEDDEF;
+  color: #2c3e50;
 }
 </style>
