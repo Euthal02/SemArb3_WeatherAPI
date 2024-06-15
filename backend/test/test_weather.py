c40a2e22992c237d69348dcbd1281521fe7aff67
mka <mkaelin@backbone.ch>
2024-06-15 16:56:04 +0200

    more backend tests and compose rename

--- backend/test/test_weather.py
+++ backend/test/test_weather.py
@@ -1,5 +1,13 @@
 from test import client
 
 
+def test_make_relay_api_call_based_on_lat_and_lon(client):
+    assert True
+
+
+def test_get_city_name_from_lat_and_lon(client):
+    assert True
+
+
 def test_weather_data_from_lattitude_and_longitude(client):
     assert True
